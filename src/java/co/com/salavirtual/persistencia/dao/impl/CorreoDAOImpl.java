c70f396c7e3d1e18cea3c87c5c7c8c054de84ea6
Scordevil <Cardenasg66@gmail.com>
2016-10-22 14:16:58 -0500

    ajuste de campos requeridos

--- src/java/co/com/salavirtual/persistencia/dao/impl/CorreoDAOImpl.java
+++ src/java/co/com/salavirtual/persistencia/dao/impl/CorreoDAOImpl.java
@@ -34,10 +34,12 @@ public class CorreoDAOImpl implements CorreoDAO {
     private void init() {
         properties.setProperty("mail.smtp.host", "smtp.gmail.com");
         properties.setProperty("mail.smtp.starttls.enable", "true");
-        properties.setProperty("mail.smtp.port", "587");
+//        properties.setProperty("mail.smtp.port", "587");
+        properties.setProperty("mail.smtp.port", "465");
         properties.put("mail.smtp.user", "sac.juguetes@gmail.com"); //Correo desde donde se enviaran lo mails
         properties.put("mail.smtp.auth", "true");
         session = Session.getDefaultInstance(properties);
+        session.setDebug(true);
 
     }
 
@@ -56,7 +58,7 @@ public class CorreoDAOImpl implements CorreoDAO {
         try {
             MimeMessage message = new MimeMessage(session);
             //quien envia
-            message.setFrom(new InternetAddress("soportelavaapp@gmail.com"));
+            message.setFrom(new InternetAddress("soportelavaapp@gmail.com", false));
             // a donde se envia
             message.addRecipient(
                     Message.RecipientType.TO,
@@ -92,23 +94,16 @@ public class CorreoDAOImpl implements CorreoDAO {
         int valor = 0;
 
         init();
-//        properties.setProperty("mail.smtp.host", "outlook.office365.com");
-//        properties.setProperty("mail.smtp.starttls.enable", "true");
-//        properties.setProperty("mail.smtp.port", "587");
-//        // properties.setProperty("mail.smtp.port", "465");
-//        properties.put("mail.smtp.user", "soportelavaapp@gmail.com"); //Correo desde donde se enviaran lo mails
-//        properties.put("mail.smtp.auth", "true");
-//        session = Session.getDefaultInstance(properties);
 
         System.out.print(usuario.toString() + " " + empresa.toString());
         try {
             MimeMessage message = new MimeMessage(session);
             //quien envia
-            message.setFrom(new InternetAddress("sac.juguetes@gmail.com"));
+            message.setFrom(new InternetAddress("sac.juguetes@gmail.com", false));
             // a donde se envia
             message.addRecipient(
                     Message.RecipientType.TO,
-                    new InternetAddress(usuario.getEmail() ));
+                    new InternetAddress(usuario.getEmail(), false));
             message.setSubject("Nuevo Usuario Administrador"); //asunto
             String mensajehtml = "<html>\n"
                     + "	<head>\n"
@@ -130,7 +125,7 @@ public class CorreoDAOImpl implements CorreoDAO {
                     + "		    <br>\n"
                     + "		    <br>\n"
                     + "		    Sus datos de acceso son: <br><br>\n"
-                    + "			Usuario: "+usuario.getUsuario()+"<br>\n"
+                    + "			Usuario: " + usuario.getUsuario() + "<br>\n"
                     + "			Login: " + usuario.getContrasena() + "\n"
                     + "			<br>\n"
                     + "		    <br>\n"
@@ -176,7 +171,8 @@ public class CorreoDAOImpl implements CorreoDAO {
                     + "						}\n"
                     + "					</style>\n"
                     + "</html>"; //Mensaje
-            message.setContent(mensajehtml, "text/html");
+//            message.setContent(mensajehtml, "text/html");
+            message.setText(mensajehtml, "ISO-8859-1", "html");
             //            message.setText("Señor(a),"
             //        + "\n"
             //         + "Le confirmamos que su registro al sistema SMS Renta fue exitoso, y le damos la bienvenida a nuestra familia. \n"
@@ -187,16 +183,14 @@ public class CorreoDAOImpl implements CorreoDAO {
             //            + "Por favor no olvide editar su perfil para crear un nombre de sesion y contraseña personalizados.\n"
             //             + "Atentamente, SMS Renta");
             Transport t = session.getTransport("smtp");
-            t.connect("smtp.gmail.com", (String) properties.get("mail.smtp.user"), "sac.123456789"); //Datos de conexion del correo de envio
+            t.connect("smtp.gmail.com", "sac.juguetes@gmail.com", "sac.123456789"); //Datos de conexion del correo de envio
             t.sendMessage(message, message.getAllRecipients());
             t.close();
             valor++;
         } catch (MessagingException me) {
             me.getMessage();
             System.out.println("error: " + me.getMessage());
-            //Aqui se deberia o mostrar un mensaje de error o en lugar
-            //de no hacer nada con la excepcion, lanzarla para que el modulo
-            //superior la capture y avise al usuario con un popup, por ejemplo.           
+
         }
         return valor;
     }
@@ -429,7 +423,7 @@ public class CorreoDAOImpl implements CorreoDAO {
     }
 
     @Override
-    public int correoConfirmacionSeleccionJuguete(Usuario_TO usuario, Inventario_TO inventario , Pedido_TO pedido) {
+    public int correoConfirmacionSeleccionJuguete(Usuario_TO usuario, Inventario_TO inventario, Pedido_TO pedido) {
         int valor = 0;
 
         init();
@@ -461,9 +455,8 @@ public class CorreoDAOImpl implements CorreoDAO {
                     + "			<center>\n"
                     + "			<header>\n"
                     + "			<div class=\"contenido\" align=\"left\">	\n"
-                    
                     + "			<br>Buenos días: " + usuario.getNombre() + " </br>\n"
-                    + "		    <br>El siguiente mensaje es para confirmar su elección de regalo para su hijo(a) "+pedido.getNombreHijo()+"  en la sala virtual. </br>\n"
+                    + "		    <br>El siguiente mensaje es para confirmar su elección de regalo para su hijo(a) " + pedido.getNombreHijo() + "  en la sala virtual. </br>\n"
                     + "		    <h2>Regalo Seleccionado:  " + inventario.getCodigo() + " " + inventario.getNombre() + " </h2>\n"
                     + "		    <br>\n"
                     + "		    <div class=\"relagalo\" align=\"center\"    >\n"
@@ -471,7 +464,7 @@ public class CorreoDAOImpl implements CorreoDAO {
                     + "		   <div class=\"texto\">\n"
                     + "		  \n"
                     + "			<fieldset>   <br><h4>Por favor imprimir y Presentar el siguiente comprobante para poder reclamar su presente.  <br>\n"
-                    + "						<br><h3>Código Único del Regalo: "+pedido.getTicket()+" </h3>\n"
+                    + "						<br><h3>Código Único del Regalo: " + pedido.getTicket() + " </h3>\n"
                     + "			 </div>\n"
                     + "			</fieldset>\n"
                     + "			  </div>\n"
@@ -479,7 +472,6 @@ public class CorreoDAOImpl implements CorreoDAO {
                     + "			   	<!--	<h5>\n"
                     + "			  		Cualquier duda o inquietud respecto al acceso o al uso de la Sala Virtual  no dude en comunicarse al área de Soporte al Cliente; allí daremos solución a todas sus dudas o inconvenientes. Puede contactarnos a través de los siguientes medios:  <br> Correo electrónico: sac.juguetes@distribuidoraelfaro.co<br>	Celular: 3186619233\n"
                     + "			  	</h5>-->\n"
-
                     + "			  </footer>	\n"
                     + "			\n"
                     + "			</header>\n"
@@ -535,7 +527,7 @@ public class CorreoDAOImpl implements CorreoDAO {
         return valor;
 
     }
-    
+
     @Override
     public int correoSeleccionJuguete(Usuario_TO usuario, Empresa_TO empresa) {
         int valor = 0;
@@ -557,67 +549,67 @@ public class CorreoDAOImpl implements CorreoDAO {
                     Message.RecipientType.TO,
                     new InternetAddress(usuario.getEmail()));
             message.setSubject("Seleccion Juguetes"); //asunto
-            String mensajehtml = "<html>\n" +
-"	<head>\n" +
-"		<meta charset=\"utf-8\">\n" +
-"		<meta name=\"viewport\" content=\"width=device-width, user-scalable=no,initial-scale=1.0,maximum-scale=1.0,minimum=1.0\">\n" +
-"		\n" +
-"		<title></title>\n" +
-"	</head>\n" +
-"	\n" +
-"	<body>\n" +
-"			<center>\n" +
-"			<header>\n" +
-"			<div class=\"contenido\" align=\"left\">	\n" +
-"			<br>Buenos días: "+usuario.getNombre()+"</br>\n" +
-"		    <br>El siguiente mensaje de correo es para informar que su acceso a la sala virtual para la elección del regalo de XXXXXXXX(Nombre del hijo o hija), XXXXXXXX(nombre del segundo hijo o hija si lo tiene; debe nombrar N cantidad de hijos que tiene esa persona) se encuentra habilitado.  </br>\n" +
-"		    <br> Haga click o copie el siguiente enlace para acceder a su sala virtual:</br>\n" +
-"		    <br>\n" +
-"		    www.XXXXXXXXX .com\n" +
-"		    <br>\n" +
-"		    <br>\n" +
-"		    Sus datos de acceso son: <br><br>\n" +
-"			Usuario: "+usuario.getUsuario()+" <br>\n" +
-"			Login: "+usuario.getContrasena()+" \n" +
-"			<br>\n" +
-"		    <br>\n" +
-"		   <div class=\"texto\">\n" +
-"		  \n" +
-"			<fieldset>   <br><h4>Con estos datos tendrá acceso al entorno de la Sala virtual de "+empresa.getNombre()+" donde podrá hacer su selección. Para nosotros es muy importante la satisfacción y felicidad de su(s) Hijo(s) y su familia. <br>\n" +
-"			 </div>\n" +
-"			</fieldset>\n" +
-"			  </div>\n" +
-"			  <footer>\n" +
-"			  	<h5>\n" +
-"			  		Cualquier duda o inquietud respecto al acceso o al uso de la Sala Virtual  no dude en comunicarse al área de Soporte al Cliente; allí daremos solución a todas sus dudas o inconvenientes. Puede contactarnos a través de los siguientes medios:  <br> Correo electrónico: sac.juguetes@distribuidoraelfaro.co<br>	Celular: 3186619233\n" +
-"			  	</h5>\n" +
-"			  </footer>	\n" +
-"			\n" +
-"			</header>\n" +
-"			</center>\n" +
-"	</body>\n" +
-"					<style type=\"text/css\">\n" +
-"						body{\n" +
-"							font-family: arial;\n" +
-"							background-color: gray 800px;\n" +
-"						    margin-top:80px;\n" +
-"							padding:90px;\n" +
-"							}\n" +
-"						.contenido{\n" +
-"\n" +
-"							align-content: left;\n" +
-"							size: 600px 800px;\n" +
-"							background-color: rgba(245, 237, 200, 0.33);\n" +
-"							}\n" +
-"						.texto{\n" +
-"							font-family: cursive;\n" +
-"						}	\n" +
-"						h5{\n" +
-"							text-shadow: 3em;\n" +
-"\n" +
-"						}\n" +
-"					</style>\n" +
-"</html>"; //Mensaje
+            String mensajehtml = "<html>\n"
+                    + "	<head>\n"
+                    + "		<meta charset=\"utf-8\">\n"
+                    + "		<meta name=\"viewport\" content=\"width=device-width, user-scalable=no,initial-scale=1.0,maximum-scale=1.0,minimum=1.0\">\n"
+                    + "		\n"
+                    + "		<title></title>\n"
+                    + "	</head>\n"
+                    + "	\n"
+                    + "	<body>\n"
+                    + "			<center>\n"
+                    + "			<header>\n"
+                    + "			<div class=\"contenido\" align=\"left\">	\n"
+                    + "			<br>Buenos días: " + usuario.getNombre() + "</br>\n"
+                    + "		    <br>El siguiente mensaje de correo es para informar que su acceso a la sala virtual para la elección del regalo de XXXXXXXX(Nombre del hijo o hija), XXXXXXXX(nombre del segundo hijo o hija si lo tiene; debe nombrar N cantidad de hijos que tiene esa persona) se encuentra habilitado.  </br>\n"
+                    + "		    <br> Haga click o copie el siguiente enlace para acceder a su sala virtual:</br>\n"
+                    + "		    <br>\n"
+                    + "		    www.XXXXXXXXX .com\n"
+                    + "		    <br>\n"
+                    + "		    <br>\n"
+                    + "		    Sus datos de acceso son: <br><br>\n"
+                    + "			Usuario: " + usuario.getUsuario() + " <br>\n"
+                    + "			Login: " + usuario.getContrasena() + " \n"
+                    + "			<br>\n"
+                    + "		    <br>\n"
+                    + "		   <div class=\"texto\">\n"
+                    + "		  \n"
+                    + "			<fieldset>   <br><h4>Con estos datos tendrá acceso al entorno de la Sala virtual de " + empresa.getNombre() + " donde podrá hacer su selección. Para nosotros es muy importante la satisfacción y felicidad de su(s) Hijo(s) y su familia. <br>\n"
+                    + "			 </div>\n"
+                    + "			</fieldset>\n"
+                    + "			  </div>\n"
+                    + "			  <footer>\n"
+                    + "			  	<h5>\n"
+                    + "			  		Cualquier duda o inquietud respecto al acceso o al uso de la Sala Virtual  no dude en comunicarse al área de Soporte al Cliente; allí daremos solución a todas sus dudas o inconvenientes. Puede contactarnos a través de los siguientes medios:  <br> Correo electrónico: sac.juguetes@distribuidoraelfaro.co<br>	Celular: 3186619233\n"
+                    + "			  	</h5>\n"
+                    + "			  </footer>	\n"
+                    + "			\n"
+                    + "			</header>\n"
+                    + "			</center>\n"
+                    + "	</body>\n"
+                    + "					<style type=\"text/css\">\n"
+                    + "						body{\n"
+                    + "							font-family: arial;\n"
+                    + "							background-color: gray 800px;\n"
+                    + "						    margin-top:80px;\n"
+                    + "							padding:90px;\n"
+                    + "							}\n"
+                    + "						.contenido{\n"
+                    + "\n"
+                    + "							align-content: left;\n"
+                    + "							size: 600px 800px;\n"
+                    + "							background-color: rgba(245, 237, 200, 0.33);\n"
+                    + "							}\n"
+                    + "						.texto{\n"
+                    + "							font-family: cursive;\n"
+                    + "						}	\n"
+                    + "						h5{\n"
+                    + "							text-shadow: 3em;\n"
+                    + "\n"
+                    + "						}\n"
+                    + "					</style>\n"
+                    + "</html>"; //Mensaje
             message.setContent(mensajehtml, "text/html");
             //            message.setText("Señor(a),"
             //        + "\n"
@@ -645,4 +637,3 @@ public class CorreoDAOImpl implements CorreoDAO {
     }
 
 }
-
